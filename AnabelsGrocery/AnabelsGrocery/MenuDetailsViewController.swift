5b4463d9d1bc5aa26e2697b36e1d4225312b843b
Raymond <raymond@markable.ai>
2023-05-03 19:41:19 -0400

    created menu detail view controller & adjusted description label

---
+++ AnabelsGrocery/AnabelsGrocery/MenuDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuDetailsViewController.swift
+//  AnabelsGrocery
+//
+//  Created by 张彦哲 on 2023/5/3.
+//
+
+import UIKit
+
+class MenuDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
